c553e84f97348fecd6ad223aac322ecdf2fc6513
Ronny <ronny@example.com>
2026-02-08 22:00:25 +0700

    Fix transaction details id not being set

--- repositories/transaction_repository.go
+++ repositories/transaction_repository.go
@@ -64,8 +64,8 @@ func (repo *transactionRepository) CreateTransaction(items []models.CheckoutItem
 
 	for i := range details {
 		details[i].TransactionID = transactionID
-		_, err = tx.Exec("INSERT INTO transaction_details (transaction_id, product_id, quantity, subtotal) VALUES ($1, $2, $3, $4)",
-			transactionID, details[i].ProductID, details[i].Quantity, details[i].Subtotal)
+		err = tx.QueryRow("INSERT INTO transaction_details (transaction_id, product_id, quantity, subtotal) VALUES ($1, $2, $3, $4) RETURNING id",
+			transactionID, details[i].ProductID, details[i].Quantity, details[i].Subtotal).Scan(&details[i].ID)
 		if err != nil {
 			return nil, err
 		}
